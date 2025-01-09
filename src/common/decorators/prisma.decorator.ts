186379731e51f23ee5048687e9ccb485598101d2
Aliph0th <theblacklord05@gmail.com>
2025-01-10 00:19:13 +0300

    refactor: move common files to lib

--- src/common/decorators/prisma.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { type ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { PrismaModels } from '../../types';
-
-export const Model = createParamDecorator((data: PrismaModels, ctx: ExecutionContext) => {
-   const request = ctx.switchToHttp().getRequest();
-   console.log(request);
-   return 2;
-});
